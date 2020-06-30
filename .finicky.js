c7d2829ca441f84bb7f9bc55b72c884753dd37bb
Josh Michielsen <github@mickey.dev>
2020-06-30 14:28:34 +0100

    clean installation, something went wonky with yadm

---
+++ .finicky.js
@@ -0,0 +1,34 @@
+module.exports = {
+	defaultBrowser: "Firefox Developer Edition",
+	handlers: [
+		{
+			match: [ /zoom.us\/j\//, /snyk.zoom.us\/j\//],
+			browser: "us.zoom.xos"
+		},
+		{
+			match: finicky.matchDomains("twitter.com"),
+			browser: "Firefox Developer Edition"
+		},
+		{
+			match: [/snyk/, finicky.matchHostnames([
+				'snyksec.atlassian.net',
+				'notion.so',
+				'*.notion.so',
+				'*.snyk-internal.net',
+				'snyk.slack.com',
+				'my.pingdom.com',
+				'zoom.us',
+				'app.logz.io'
+			])],
+			browser: "Firefox",
+		},
+		{
+			match: finicky.matchDomains("open.spotify.com"),
+			browser: "Spotify"
+		},
+		{
+			match: ({ keys }) => keys.option,
+			browser: "Firefox",
+		}
+	]
+}
