85cadff47443943990248fcdb3d6e29979847f6f
Josh Michielsen <github@mickey.dev>
2024-03-10 18:31:08 +0000

    improve fzf-tab functionality, add preview script

--- .finicky.js
+++ .finicky.js
@@ -1,38 +1,44 @@
 module.exports = {
-	defaultBrowser: "Firefox Developer Edition",
-	options: {
-	  hideIcon: false,
-	},
-	handlers: [
-		{
-			match: [ /zoom.us\/j\//, /weaveworks.zoom.us\/j\//],
-			browser: "us.zoom.xos"
-		},
-		{
-			match: finicky.matchDomains([
-			  "twitter.com",
-			  "linkedin.com"
-			]),
-			browser: "Firefox Developer Edition"
-		},
-		{
-			match: [/weaveworks/, /open-component-model/, finicky.matchHostnames([
-				'zenhub.com',
-				'notion.so',
-				'*.notion.so',
-				'weaveworks.slack.com',
-				'docs.google.com',
-				'meet.google.com'
-			])],
-			browser: "Google Chrome",
-		},
-		{
-			match: finicky.matchDomains("open.spotify.com"),
-			browser: "Spotify"
-		},
-		{
-			match: () => finicky.getKeys().option,
-			browser: "Google Chrome",
-		}
-	]
-}
+  defaultBrowser: "Firefox Developer Edition",
+  options: {
+    hideIcon: false,
+  },
+  handlers: [
+    {
+      match: [/zoom.us\/j\//, /dataminr.zoom.us\/j\//],
+      browser: "us.zoom.xos",
+    },
+    {
+      match: finicky.matchDomains(["twitter.com", "linkedin.com"]),
+      browser: "Firefox Developer Edition",
+    },
+    {
+      match: [
+        /dataminr/,
+        /amazonaws.com/,
+        /dmnr.io/,
+        /http:\/\/127\.0\.0\.1:[456][0-9]*\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/,
+        finicky.matchHostnames([
+          "dataminr.atlassian.net",
+          "notion.so",
+          "*.notion.so",
+          "dataminr.slack.com",
+          "docs.google.com",
+          "meet.google.com",
+          "app.scaleft.com",
+          "dmcorp.okta.com",
+          "spacelift.io",
+        ]),
+      ],
+      browser: "Google Chrome",
+    },
+    {
+      match: finicky.matchDomains("open.spotify.com"),
+      browser: "Spotify",
+    },
+    {
+      match: () => finicky.getKeys().option,
+      browser: "Google Chrome",
+    },
+  ],
+};
